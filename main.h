e6e91a2e9acdcfb99a7441ccfb6e56e9e10ddcd8
FroggyElder <shadowskinC@gmail.com>
2024-08-08 19:24:24 +0800

    some comments

--- main.h
+++ main.h
@@ -18,14 +18,31 @@
 
 namespace dev
 {
+    ///path to ur lcd device
     #define LCD0 "/dev/fb0"
+    //path to ur touchScreen
     #define TS0 "/dev/input/event0"
+    //path to ur led
     #define LED0 "/dev/Led"
 }
 #define ARGB_BLACK 0xff000000
 
+//simple function to delay for milliseconds
 void delay(int milliseconds);
+
+//initiate random seed with millisecond time
 void seedInit ();
+
+//thread to handle fall and delay of object
 void fallThread (board::BlockBoard* board);
+
+//terminate the whole program (through pauseMenu)
+void fullexit (void* pauseMenu);
+
+//unlock the mutex lock
+void mutexUnlock (void* mutex);
+
+//reset board
+void boardRestart (void* board)
 
 #endif
