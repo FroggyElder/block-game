4059ef89046fe86200b3e8a167dcbb502fdafc94
FroggyElder <shadowskinC@gmail.com>
2024-08-09 18:58:09 +0800

    ,wwqgf

--- src/observer/observer.h
+++ src/observer/observer.h
@@ -3,6 +3,7 @@
 
 #include <algorithm>
 #include <vector>
+#include <mutex>
 
 namespace obs
 {
@@ -14,19 +15,31 @@ public:
 
 class Subject {
 private:
-    std::vector<Observer*> observers;
+    std::vector<Observer*> m_observers;
+    std::mutex m_lock{};
 
 public:
-    void addObserver(Observer* observer) {
-        observers.push_back(observer);
+    bool addObserver(Observer* observer) {
+        std::lock_guard<std::mutex> lock(m_lock);
+
+        if (std::find(m_observers.begin(), m_observers.end(), observer) == m_observers.end()) {
+            m_observers.push_back(observer);
+            return true;
+        }
     }
 
-    void removeObserver(Observer* observer) {
-        observers.erase(std::remove(observers.begin(), observers.end(), observer), observers.end());
+    bool removeObserver(Observer* observer) {
+        std::lock_guard<std::mutex> lock(m_lock);
+
+        auto it = std::remove(m_observers.begin(), m_observers.end(), observer);
+        if (it != m_observers.end()) {
+            m_observers.erase(it, m_observers.end());
+            return true;
+        }
     }
 
     void notifyObservers() {
-        for (Observer* observer : observers) {
+        for (Observer* observer : m_observers) {
             observer->update();
         }
     }
